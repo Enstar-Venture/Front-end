3f92d55767ea5285c32fab1ab2ce34fb1965df39
cynic <cynic2001@gmail.com>
2022-12-30 22:26:57 +0800

    Add Header and Footer

---
+++ enstar/src/components/TheFooter.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="q-pa-md">
+    <div class="row items-start justify-between">
+      <template v-for="footerLink of footerLinks">
+        <div class="col-2">
+          <div class="text-h5 q-pl-sm">{{footerLink.label}}</div>
+          <div class="column items-start ">
+            <template v-for="item of footerLink.items">
+              <q-btn flat :label="item.label" :to="item.link"/>
+            </template>
+          </div>
+
+        </div>
+      </template>
+      <div class="col-3 column items-end">
+        <div class="q-mr-lg q-pt-sm">
+          <img alt="logo" src="LOGO2.jpg" height="50">
+        </div>
+        <div class="text-h6">
+          Slogan Slogan Slogan
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+const footerLinks = [
+  {
+    label: "Company",
+    items: [
+      {
+        label: "Careers",
+        link: "careers"
+      },
+      {
+        label: "About us",
+        link: "about"
+      }
+    ]
+  },
+  {
+    label: "Resources",
+    items: [
+      {
+        label: "Projects",
+        link: "projects"
+      },
+      {
+        label: "Your wallet",
+        link: "wallet"
+      }
+    ]
+  },
+  {
+    label: "Resources",
+    items: [
+      {
+        label: "Projects",
+        link: "projects"
+      },
+      {
+        label: "Your wallet",
+        link: "wallet"
+      }
+    ]
+  },
+  {
+    label: "Resources",
+    items: [
+      {
+        label: "Projects",
+        link: "projects"
+      },
+      {
+        label: "Your wallet",
+        link: "wallet"
+      }
+    ]
+  }
+]
+export default defineComponent({
+  name: "TheFooter",
+  setup () {
+    return {
+      footerLinks
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- enstar/src/layouts/MainLayout.vue
+++ enstar/src/layouts/MainLayout.vue
@@ -1,114 +1,51 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <div class="q-mr-lg q-pt-sm">
+          <img alt="logo" src="LOGO2.jpg" height="50">
+        </div>
+        <div class="q-px-md">
+          <q-btn label="Projects" to="projects"/>
+        </div>
+        <div class="q-px-md">
+          <q-btn label="Your Wallet" to="wallet"/>
+        </div>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title/>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div class="q-px-md">
+          <q-btn label="Connect Wallet" @click="connect"/>
+        </div>
+
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
+      <the-footer />
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import  TheFooter  from 'components/TheFooter.vue'
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
+    TheFooter
   },
 
   setup () {
-    const leftDrawerOpen = ref(false)
-
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      connect() {
+        console.log("connect wallet")
       }
     }
   }
