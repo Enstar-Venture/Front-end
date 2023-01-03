c6460b9046deeb5a262b16c430763e3115dfa377
cynic <cynic2001@gmail.com>
2023-01-03 15:35:23 +0800

    Add First Page

--- enstar/src/pages/IndexPage.vue
+++ enstar/src/pages/IndexPage.vue
@@ -1,10 +1,38 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <q-card class="my-card" flat bordered style="width: 100%">
+      <q-card-section horizontal>
+        <q-card-section class="col-7">
+           <div class="q-ma-xl q-pa-xl">
+             <div class="text-h2">
+               Get early access to the ideas of tomorrow
+             </div>
+             <div class="q-pt-xl text-h5">
+               Highly-vetted Web3 projects you can trust. Supported by industry-leading creators and funds.
+             </div>
+
+           </div>
+        </q-card-section>
+
+        <q-img
+          class="col-5"
+          src="https://cdn.quasar.dev/img/parallax2.jpg"
+        />
+      </q-card-section>
+    </q-card>
+    <div class="q-my-lg row flex-center q-gutter-xl">
+      <template v-for="i in 4">
+        <q-card class="col-2 text-center">
+          <q-card-section class="text-h4">
+            121
+            <div class="text-h5">
+              Ideas Funded
+            </div>
+          </q-card-section>
+        </q-card>
+      </template>
+
+    </div>
   </q-page>
 </template>
 
